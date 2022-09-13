31273b17f30fa4ec3d848d2fa5e3e3dbcc36860c
Ronald Moreno H <ronald.27unsch@gmail.com>
2022-09-13 09:16:43 -0500

    hfghfhngj

--- src/main/java/com/apirest/exams/MvcConfig.java
+++ src/main/java/com/apirest/exams/MvcConfig.java
@@ -19,7 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/v1/**") 
                 
-                 .allowedOrigins( "http://192.168.100.5:4200","https://fractal-test-a4b45.web.app" ) // CAMBIAR CON DOMINIO DEL FRONTEND 
+                 .allowedOrigins( "https://fractal-test-a4b45.web.app" ) // CAMBIAR CON DOMINIO DEL FRONTEND 
                  .allowedMethods("GET","POST","PUT","DELETE") 
                  .allowedHeaders("*")
 	           	 .maxAge(3600);
